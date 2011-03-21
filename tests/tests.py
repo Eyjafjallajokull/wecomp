65fc68943f3563048968c17c48edfb361cb07b46
Eyjafjallajokull <pawel.olejniczak@gmail.com>
2011-03-21 22:33:10 +0100

    add test

--- tests/tests.py
+++ tests/tests.py
@@ -1,23 +1,30 @@
 import unittest
 import subprocess
+
 class TestSampleFiles(unittest.TestCase):
   
-  def setUp(self):
-    pass
-  
-  def test1(self):
-    self.commonFileTest('test1')
-    
-    
-  def commonFileTest(self, test):
+  def test1stdout(self):
+    test = 'test1'
     testfile = test+'.html'
     cmd = '../wecomp %s' % testfile
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
     teststring = p.communicate()[0]
     
-    expectedString = open(test+'.output.html', 'r').read();
+    self.commonFileTest(teststring, test)
+  
+  def test1fileout(self):
+    test = 'test1'
+    testfile = test+'.html'
+    subprocess.Popen('rm tmp > /dev/null 2>&1', shell=True).wait()
+    cmd = '../wecomp %s --output tmp' % testfile
+    subprocess.Popen(cmd, shell=True).wait()
+    teststring = open('tmp', 'r').read()
     
-    self.assertEqual(teststring, expectedString);
+    self.commonFileTest(teststring, test)
+    
+  def commonFileTest(self, teststring, test):
+    expectedString = open(test+'.output.html', 'r').read()
+    self.assertEqual(teststring, expectedString)
 
 if __name__ == '__main__':
     unittest.main()
