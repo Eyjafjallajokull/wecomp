fb3fd670f2839cdffda6c2b2397beb554b8880da
pawel <pawel.olejniczak@gmail.com>
2012-03-10 21:29:06 +0100

    use single quotes

--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ from distutils.core import setup
 from subprocess import Popen
 
 _top_dir = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(_top_dir, "lib"))
+sys.path.insert(0, os.path.join(_top_dir, 'lib'))
 try:
     import wecomp
 finally:
     del sys.path[0]
 
-classifiers = """\
+classifiers = '''\
 Development Status :: 4 - Beta
 Environment :: Console
 Intended Audience :: Developers
@@ -25,15 +25,15 @@ Topic :: Software Development :: Code Generators
 Topic :: Software Development :: Compilers
 Topic :: Text Processing :: Markup :: HTML
 Topic :: Utilities
-"""
-long_description = """\
+'''
+long_description = '''\
 Pack and compress client-side source code.
 
 Features:
  * CSS, JS -- minification and merging files
  * HTML -- minification with proper handling of script and style tags
  * PHP -- same as HTML, but PHP code is left untouched
-"""
+'''
 
 
 if sys.version < '2.2.3':
@@ -42,19 +42,19 @@ if sys.version < '2.2.3':
     DistributionMetadata.download_url = None
 
 setup(
-    name="wecomp",
+    name='wecomp',
     version=wecomp.__version__,
-    maintainer="Pawel Olejniczak",
-    maintainer_email="pawel.olejniczak@gmail.com",
-    author="Pawel Olejniczak",
-    author_email="pawel.olejniczak@gmail.com",
-    url="https://github.com/Eyjafjallajokull/wecomp",
-    license="MIT",
-    platforms=["linux"],
-    package_dir={"": "lib"},
-    py_modules=["wecomp"],
-    scripts=["wecomp"],
-    description="Pack and compress client-side source code.",
-    classifiers=filter(None, classifiers.split("\n")),
+    maintainer='Pawel Olejniczak',
+    maintainer_email='pawel.olejniczak@gmail.com',
+    author='Pawel Olejniczak',
+    author_email='pawel.olejniczak@gmail.com',
+    url='https://github.com/Eyjafjallajokull/wecomp',
+    license='GPL',
+    platforms=['linux'],
+    package_dir={'': 'lib'},
+    py_modules=['wecomp'],
+    scripts=['wecomp'],
+    description='Pack and compress client-side source code.',
+    classifiers=filter(None, classifiers.split('\n')),
     long_description=long_description,
 )
